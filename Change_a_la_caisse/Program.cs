1a58974a7252720991dd7a1c0dececd5d0f4df02
TheRealDAZL <0667750@cegepoutaouais.qc.ca>
2023-11-17 04:23:40 -0500

    Version optimisée

--- Change_a_la_caisse/Program.cs
+++ Change_a_la_caisse/Program.cs
@@ -2,10 +2,10 @@
 // arrondi aux 5 cents près, de façon à donner le moins d'espèces de monnaie que possible
 
 // Déclarer les constantes
-double[] VALEUR = new double[10] {100, 50, 20, 10, 5, 2, 1, 0.25, 0.10, 0.05};
+int[] COUPURES_EN_CENTS = new int[10] {10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5};
 
 // Déclarer les variables
-double[] monnaie = new double[10];
+int[] monnaie = new int[10];
 string continuer = "";
 
 // Tant que la variable resultat n'est pas égale à "non", on exécute le code qui suit
@@ -15,29 +15,26 @@ do
     try
     {
         // Déclarer et initialiser la variable
-        double montant_total = 0;
+        double montant_en_cents = 0;
 
         // Entrer la valeur du montant d'argent
         Console.WriteLine("Entrez le montant d’argent à décomposer en coupures, dans le format 0,00 :");
 
-        // Définir la variable
-        montant_total = double.Parse(Console.ReadLine());
-
-        // Tronquer le montant total à une valeure entière après l'avoir transformé en cents
-        double montant_en_cents = Math.Truncate(montant_total * 100);
+        // Définir la variable, et tronquer le montant total à une valeure entière après l'avoir transformé en cents
+        montant_en_cents = double.Parse(Console.ReadLine()) * 100;
 
         // Trouver le montant arrondi aux 5 cents près
         double montant_a_decomposer = montant_en_cents - montant_en_cents % 5 + Math.Round((montant_en_cents % 5) / 5) * 5;
 
         // Afficher la ligne suivante
-        Console.WriteLine("\nPour avoir le moins de billets et de pièces possible avec " + montant_total.ToString("0.00") + "$, arrondi aux 5 cents près, il faut décomposer le montant total selon les coupures suivantes :");
+        Console.WriteLine("\nPour avoir le moins de billets et de pièces possible avec " + (montant_en_cents / 100).ToString("0.00") + "$, arrondi aux 5 cents près, il faut décomposer le montant total selon les coupures suivantes :");
 
         // Calculer les quantités de chaque coupure de monnaie, puis afficher chaque résultat
-        for (int compteur = 0; compteur < VALEUR.Length; compteur++)
+        for (int compteur_de_coupures = 0; compteur_de_coupures < COUPURES_EN_CENTS.Length; compteur_de_coupures++)
         {
-            monnaie[compteur] = (montant_a_decomposer - montant_a_decomposer % (VALEUR[compteur] * 100)) / (VALEUR[compteur] * 100);
-            montant_a_decomposer = montant_a_decomposer - monnaie[compteur] * (VALEUR[compteur] * 100);
-            Console.WriteLine(monnaie[compteur] + " * " + VALEUR[compteur].ToString("0.00") + "$");
+            monnaie[compteur_de_coupures] = (int)((montant_a_decomposer - montant_a_decomposer % COUPURES_EN_CENTS[compteur_de_coupures]) / COUPURES_EN_CENTS[compteur_de_coupures]);
+            montant_a_decomposer = montant_a_decomposer - monnaie[compteur_de_coupures] * COUPURES_EN_CENTS[compteur_de_coupures];
+            Console.WriteLine(monnaie[compteur_de_coupures] + " * " + (COUPURES_EN_CENTS[compteur_de_coupures] / (double)100).ToString("0.00") + "$");
         }
     }
 
@@ -59,8 +56,11 @@ do
     // Lire la réponse
     continuer = Console.ReadLine();
 
-    // Si la variable continuer est non vide, alors on prend le premier caractère et on le met en minuscule
-    if (continuer != "") continuer = continuer.Substring(0, 1).ToLower();
+    // Si la variable continuer n'est pas vide, alors on prend le premier caractère et on le met en minuscule
+    if (continuer != "")
+    {
+        continuer = continuer.Substring(0, 1).ToLower();
+    }
 
     // Afficher le message
     Console.Write("\n");
