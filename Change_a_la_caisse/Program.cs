a4bf0dfb0149ab631132c0b905c2b2f2e78d5784
TheRealDAZL <0667750@cegepoutaouais.qc.ca>
2023-11-19 11:16:44 -0500

    Programme mieux optimisé, et avec plus de commentaires

--- Change_a_la_caisse/Program.cs
+++ Change_a_la_caisse/Program.cs
@@ -2,55 +2,71 @@
 // arrondi aux 5 cents près, de façon à donner le moins d'espèces de monnaie que possible
 
 // Déclarer les constantes
-int[] COUPURES_EN_CENTS = new int[10] {10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5};
+int[] COUPURES_EN_CENTS = new int[10] { 10000, 5000, 2000, 1000, 500, 200, 100, 25, 10, 5 };
 
 // Déclarer les variables
-int[] monnaie = new int[10];
+int[] monnaie = new int[COUPURES_EN_CENTS.Length];
 string continuer = "";
 
-// Tant que la variable resultat n'est pas égale à "non", on exécute le code qui suit
+// Effectuer les instructions suivantes, puis répéter la boucle tant que la valeur d ela variable resultat n'est pas égale à "non"
 do
 {
-    // Exécuter le bloc de code qui suit, en cherchant pour des erreurs
-    try
+    // Déclarer et initialiser les variables
+    double montant_en_cents = -1;
+    double montant_a_decomposer = -1;
+
+    // Effectuer les opérations suivantes, tant que montant_en_cents est un nombre négatif
+    while (montant_en_cents < 0)
     {
-        // Déclarer et initialiser la variable
-        double montant_en_cents = 0;
-
-        // Entrer la valeur du montant d'argent
-        Console.WriteLine("Entrez le montant d’argent à décomposer en coupures, dans le format 0,00 :");
-
-        // Définir la variable, et tronquer le montant total à une valeure entière après l'avoir transformé en cents
-        montant_en_cents = double.Parse(Console.ReadLine()) * 100;
-
-        // Trouver le montant arrondi aux 5 cents près
-        double montant_a_decomposer = montant_en_cents - montant_en_cents % 5 + Math.Round((montant_en_cents % 5) / 5) * 5;
-
-        // Afficher la ligne suivante
-        Console.WriteLine("\nPour avoir le moins de billets et de pièces possible avec " + (montant_en_cents / 100).ToString("0.00") + "$, arrondi aux 5 cents près, il faut décomposer le montant total selon les coupures suivantes :");
-
-        // Calculer les quantités de chaque coupure de monnaie, puis afficher chaque résultat
-        for (int compteur_de_coupures = 0; compteur_de_coupures < COUPURES_EN_CENTS.Length; compteur_de_coupures++)
+        // Exécuter le bloc de code qui suit, en cherchant pour des erreurs
+        try
         {
-            monnaie[compteur_de_coupures] = (int)((montant_a_decomposer - montant_a_decomposer % COUPURES_EN_CENTS[compteur_de_coupures]) / COUPURES_EN_CENTS[compteur_de_coupures]);
-            montant_a_decomposer = montant_a_decomposer - monnaie[compteur_de_coupures] * COUPURES_EN_CENTS[compteur_de_coupures];
-            Console.WriteLine(monnaie[compteur_de_coupures] + " * " + (COUPURES_EN_CENTS[compteur_de_coupures] / (double)100).ToString("0.00") + "$");
+            // Afficher ce message
+            Console.WriteLine("Entrez le montant d’argent à décomposer en coupures, dans le format 0,00 :");
+
+            // Définir la variable, et tronquer le montant total à une valeure entière après l'avoir transformé en cents
+            montant_en_cents = double.Parse(Console.ReadLine()) * 100;
+
+            // Trouver le montant arrondi aux 5 cents près
+            montant_a_decomposer = montant_en_cents - montant_en_cents % 5 + Math.Round((montant_en_cents % 5) / 5) * 5;
+        }
+
+        // S'il y a une erreur dans le bloc de code précédent, afficher ce message
+        catch (FormatException)
+        {
+            Console.WriteLine("Cette entrée n'est pas valide.\n");
+        }
+
+        // S'il y a une erreur dans le bloc de code précédent, afficher ce message
+        catch (System.OverflowException)
+        {
+            Console.WriteLine("Cette entrée est trop large. Entrez un nombre dont la valeur est plus petite que celle que vous venez d'entrer.\n");
         }
     }
 
-    // S'il y a une erreur dans le bloc de code précédent, afficher ce message
-    catch (FormatException)
+    // Afficher la ligne suivante
+    Console.WriteLine("\nPour avoir le moins de billets et de pièces possible avec " + (montant_en_cents / 100).ToString("0.00") + "$, arrondi aux 5 cents près, il faut décomposer le montant total dans les coupures suivantes :");
+
+    // Calculer les quantités de chaque coupure de monnaie, puis afficher chaque résultat
+    for (int compteur_de_coupures = 0; compteur_de_coupures < COUPURES_EN_CENTS.Length; compteur_de_coupures++)
     {
-        Console.WriteLine("Cette entrée n'est pas valide.");
+        // Effectuer les opérations suivantes afin de mémoriser la quantité de monnaie dans une coupure donnée :
+        // Étape 1 -> Prendre le montant_a_decomposer et lui soustraire le résultat de l'opération montant_a_decomposer % COUPURES_EN_CENTS[compteur_de_coupures].
+        // Cela va nous donner un nombre qui, lorsque divisé par COUPURES_EN_CENTS[compteur_de_coupures], nous donnera un résultat entier;
+        // Étape 2 -> Diviser le résultat de l'étape 1 par la valeur de COUPURES_EN_CENTS[compteur_de_coupures];
+        // Étape 3 -> Convertir le résultat de l'étape 2 (qui se doit d'être un nombre entier) en variable int;
+        // Étape 4 -> Mémoriser le résultat de l'étape 3 dans la variable monnaie[compteur_de_coupures], qui est une variable int[].
+        monnaie[compteur_de_coupures] = (int)((montant_a_decomposer - montant_a_decomposer % COUPURES_EN_CENTS[compteur_de_coupures]) / COUPURES_EN_CENTS[compteur_de_coupures]);
+
+        // Prendre le montant_a_decomposer et lui soustraire le résultat de l'opération monnaie[compteur_de_coupures] * COUPURES_EN_CENTS[compteur_de_coupures].
+        // Mémoriser le résultat dans la variable montant_a_decomposer.
+        montant_a_decomposer = montant_a_decomposer - monnaie[compteur_de_coupures] * COUPURES_EN_CENTS[compteur_de_coupures];
+
+        // Afficher ce message
+        Console.WriteLine(monnaie[compteur_de_coupures] + " * " + (COUPURES_EN_CENTS[compteur_de_coupures] / (double)100).ToString("0.00") + "$");
     }
 
-    // S'il y a une erreur dans le bloc de code précédent, afficher ce message
-    catch (System.OverflowException)
-    {
-        Console.WriteLine("Cette entrée est trop large. Entrez un nombre dont la valeur absolue est plus petite que celle que vous venez d'entrer.");
-    }
-
-    // Afficher le message
+    // Afficher ce message
     Console.WriteLine("\nVoulez-vous continuer? Tapez \"n\" (pour non), puis appuyez sur la touche Entrée pour terminer le programme.");
 
     // Lire la réponse
@@ -62,6 +78,6 @@ do
         continuer = continuer.Substring(0, 1).ToLower();
     }
 
-    // Afficher le message
+    // Afficher ce message
     Console.Write("\n");
 } while (continuer != "n");
